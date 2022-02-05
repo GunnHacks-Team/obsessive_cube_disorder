a544710e1dc4855d61ddbb933f46f565a5b1c562
Michael Ibrahim <mike.a.ibrahim@gmail.com>
2022-02-05 15:12:27 -0800

    matrix operations

--- src/ml.js
+++ src/ml.js
@@ -1,12 +1,11 @@
 class Matrix {
-	constructor(rows, cols, defaultValue=0, data=null) {
+	constructor(rows, cols, defaultValue = 0, data = null) {
 		if (data == null) {
 			this.data = [];
 			for (let i = 0; i < rows; i++) {
 				this.data.push([]);
-				for (let j = 0; j < cols; j++) {
+				for (let j = 0; j < cols; j++)
 					this.data[i][j] = defaultValue;
-				}
 			}
 		} else {
 			this.data = data;
@@ -15,47 +14,71 @@ class Matrix {
 		this.cols = this.data[0].length;
 	}
 
-	equalDimensions(other) { return this.rows == other.rows && this.cols == other.cols; }
+	equalDimensions(other) {
+		return this.rows == other.rows && this.cols == other.cols;
+	}
 	add(other) {
 		let result = new Matrix(this.rows, this.cols);
 		if (other instanceof Matrix) {
 			if (!this.equalDimensions(other))
 				throw new Error("Matrix dimensions must be the same when adding: " + this.rows + "x" + this.cols + " vs " + other.rows + "x" + other.cols);
 			for (let i = 0; i < this.rows; i++)
-				for (let j = 0; j < this.cols; j++) result.data[i][j] = this.data[i][j] + other.data[i][j];
+				for (let j = 0; j < this.cols; j++)
+					result.data[i][j] = this.data[i][j] + other.data[i][j];
 			return result;
 		} else if (typeof other === "number") {
 			for (let i = 0; i < this.rows; i++)
-				for (let j = 0; j < this.cols; j++) result.data[i][j] = this.data[i][j] + other;
+				for (let j = 0; j < this.cols; j++)
+					result.data[i][j] = this.data[i][j] + other;
 			return result;
-		} else {
-			throw new Error("Cannot add matrix and " + typeof other);
 		}
+		throw new Error("Cannot add matrix and " + typeof other);
 	}
-
 	multiply(other) {
 		let result = new Matrix(this.rows, this.cols);
 		if (other instanceof Matrix) {
-			if (!this.equalDimensions(other)) {
+			if (!this.equalDimensions(other))
 				throw new Error("Matrix dimensions must be the same when multiplying: " + this.rows + "x" + this.cols + " vs " + other.rows + "x" + other.cols);
-			}
-			for (let i = 0; i < this.rows; i++) {
-				for (let j = 0; j < this.cols; j++) {
+			for (let i = 0; i < this.rows; i++)
+				for (let j = 0; j < this.cols; j++)
 					result.data[i][j] = this.data[i][j] * other.data[i][j];
-				}
-			}
 			return result;
 		} else if (typeof other === "number") {
+			for (let i = 0; i < this.rows; i++)
+				for (let j = 0; j < this.cols; j++)
+					result.data[i][j] = this.data[i][j] * other;
+			return result;
+		}
+		throw new Error("Cannot multiply matrix and " + typeof other);
 	}
 	subtract(other) {
-		if (!this.equalDimensions(other)) {
-			throw new Error("Matrix dimensions must be the same when subtracting: " + this.rows + "x" + this.cols + " vs " + other.rows + "x" + other.cols);
+		if (other instanceof Matrix) {
+			if (!this.equalDimensions(other))
+				throw new Error("Matrix dimensions must be the same when subtracting: " + this.rows + "x" + this.cols + " vs " + other.rows + "x" + other.cols);
+			return this.add(other.multiply(-1));
+		} else if (typeof other === "number") {
+			return this.add(-other);
 		}
-		// call add but multiply with a negative number
-		return this.add(other.multiply(-1));
+		throw new Error("Cannot subtract matrix and " + typeof other);
 	}
-
-	printData(name=null) {
+	divide(other) {
+		if (other instanceof Matrix) {
+			if (!this.equalDimensions(other))
+				throw new Error("Matrix dimensions must be the same when dividing: " + this.rows + "x" + this.cols + " vs " + other.rows + "x" + other.cols);
+			return this.multiply(other.inverse());
+		} else if (typeof other === "number") {
+			return this.multiply(1 / other);
+		}
+		throw new Error("Cannot divide matrix and " + typeof other);
+	}
+	inverse() {
+		let result = new Matrix(this.rows, this.cols);
+		for (let i = 0; i < this.rows; i++)
+			for (let j = 0; j < this.cols; j++)
+				result.data[i][j] = 1 / this.data[i][j];
+		return result;
+	}
+	printData(name = null) {
 		let out = "";
 		if (name != null) out += name + " " + this.data.length + "x" + this.data[0].length + ":\n";
 		else out += "Matrix " + this.data.length + "x" + this.data[0].length + ":\n";
@@ -68,11 +91,6 @@ class Matrix {
 }
 
 let a = new Matrix(2, 3, 4);
-let b = new Matrix(2, 3, 5);
-a.printData("a");
-let c = a.add(b);
-c.printData("a + b");
-let d = a.subtract(b);
-d.printData("a - b");
+a.divide(2).printData("a");
 
 export default Matrix;
