5af7aedf2d0b41c927478ead8620ed8219ac93bf
Michael Ibrahim <mike.a.ibrahim@gmail.com>
2022-02-06 10:33:10 -0800

    obtained a side

--- src/ml.js
+++ src/ml.js
@@ -16,6 +16,9 @@ export class Matrix {
 	equalDimensions(other) {
 		return this.rows == other.rows && this.cols == other.cols;
 	}
+	get(index) {
+		return this.data[Math.floor(index / this.cols)][index % this.cols];
+	}
 	add(other) {
 		let result = new Matrix(this.rows, this.cols);
 		if (other instanceof Matrix) {
@@ -116,16 +119,70 @@ export class Matrix {
 				if (this.data[i][j] > max) max = this.data[i][j];
 		return max;
 	}
+	maxDim(dim) {
+		if (dim == "row") {
+			// return the max along each collumn
+			let result = new Matrix(1, this.cols);
+			let index = new Matrix(1, this.cols);
+			for (let i = 0; i < this.cols; i++) {
+				for (let j = 0; j < this.rows; j++) {
+					if (this.data[j][i] > result.data[0][i]) {
+						result.data[0][i] = this.data[j][i];
+						index.data[0][i] = j;
+					}
+				}
+			}
+			return { result, index };
+		} else if (dim == "col") {
+			// return the max along each row
+			let result = new Matrix(this.rows, 1);
+			let index = new Matrix(this.rows, 1);
+			for (let i = 0; i < this.rows; i++) {
+				for (let j = 0; j < this.cols; j++) {
+					if (this.data[i][j] > result.data[i][0]) {
+						result.data[i][0] = this.data[i][j];
+						index.data[i][0] = j;
+					}
+				}
+			}
+			return { result, index };
+		}
+	}
+	maxFrequency() {
+		let result = new Matrix(1, 1);
+
+		let freq = {};
+		for (let i = 0; i < this.rows; i++) {
+			for (let j = 0; j < this.cols; j++) {
+				if (freq[this.data[i][j]]) freq[this.data[i][j]]++;
+				else freq[this.data[i][j]] = 1;
+			}
+		}
+
+		let max = 0;
+		let maxIndex = 0;
+		for (let i in freq) {
+			if (freq[i] > max) {
+				max = freq[i];
+				maxIndex = i;
+			}
+		}
+
+		result.data[0][0] = maxIndex;
+		return result;
+	}
+		
+
+
 	oneHot() {
+		let max = Number(this.max()) + 1;
 		if (this.rows == 1) {
-			let max = this.max();
-			let result = new Matrix(max + 1, this.cols);
+			let result = new Matrix(max, this.cols);
 			for (let i = 0; i < this.cols; i++)
 				result.data[this.data[0][i]][i] = 1;
 			return result;
 		} else if (this.cols == 1) {
-			let max = this.max();
-			let result = new Matrix(this.rows, max + 1);
+			let result = new Matrix(this.rows, max);
 			for (let i = 0; i < this.rows; i++)
 				result.data[i][this.data[i][0]] = 1;
 			return result;
@@ -188,26 +245,36 @@ export class Matrix {
 		return result / (this.rows * this.cols);
 	}
 	unravel() {
-		let result = [];
+		let result = new Matrix(this.rows * this.cols, 1);
 		for (let i = 0; i < this.rows; i++)
 			for (let j = 0; j < this.cols; j++)
-				result.push(this.data[i][j]);
+				result.data[i * this.cols + j][0] = this.data[i][j];
 		return result;
 	}
-	ravel(rows, cols) {
+	reshape(rows, cols) {
+		if (rows * cols != this.rows * this.cols)
+			throw new Error("Cannot reshape matrix to " + rows + "x" + cols + " from " + this.rows + "x" + this.cols);
 		let result = new Matrix(rows, cols);
-		for (let i = 0; i < rows; i++)
-			for (let j = 0; j < cols; j++)
-				result.data[i][j] = this.data[0][i * cols + j];
+		for (let i = 0; i < rows; i++) {
+			for (let j = 0; j < cols; j++) {
+				result.data[i][j] = this.get(i * cols + j);
+			}
+		}
 		return result;
 	}
-		
+	slice(rowMin, rowMax, colMin, colMax) {
+		let result = new Matrix(rowMax - rowMin, colMax - colMin);
+		for (let i = rowMin; i < rowMax; i++)
+			for (let j = colMin; j < colMax; j++)
+				result.data[i - rowMin][j - colMin] = this.data[i][j];
+		return result;
+	}
 	print(name = null) {
 		let out = "";
-		if (name != null) out += name + " " + this.data.length + "x" + this.data[0].length + ":\n";
-		else out += "Matrix " + this.data.length + "x" + this.data[0].length + ":\n";
-		for (let i = 0; i < this.data.length; i++) {
-			for (let j = 0; j < this.data[i].length; j++) out += this.data[i][j] + " ";
+		if (name != null) out += name + " " + this.rows + "x" + this.cols + ":\n";
+		else out += "Matrix " + this.rows + "x" + this.cols + ":\n";
+		for (let i = 0; i < this.rows; i++) {
+			for (let j = 0; j < this.cols; j++) out += this.data[i][j] + " ";
 			out += "\n";
 		}
 		console.log(out);
@@ -225,7 +292,7 @@ export class MulticlassClassifier {
 		this.y = y;
 		this.structure = structure;
 		if (this.y != null && this.structure[this.structure.length - 1] != this.y.rows)
-			throw new Error("Last layer size must be equal to number of classes");
+			throw new Error("Last layer size must be equal to number of classes: " + this.structure[this.structure.length - 1] + " != " + this.y.rows);
 		this.A = [];
 		this.W = [];
 		this.B = [];
@@ -279,6 +346,9 @@ export class MulticlassClassifier {
 			}
 		}
 	}
+	predict(X) {
+		return this.forward(X);
+	}
 	mse(Y) {
 		let mse = 0;
 		for (let i = 0; i < Y.rows; i++)
@@ -286,24 +356,32 @@ export class MulticlassClassifier {
 				mse += Math.pow(this.A[this.A.length - 1].data[i][j] - Y.data[i][j], 2);
 		return mse / (Y.rows * Y.cols);
 	}
-	async loadParams() {
-		return new Promise((resolve, reject) => {
-			fetch('./weight.json')
-				.then(response => {
-					return response.json();
-				})
-				.then(data => {
-					this.setWeightData(data);
-					fetch('./bias.json')
-						.then(response => {
-							return response.json();
-						})
-						.then(data => {
-							this.setBiasData(data);
-							resolve();
-						});
-				});
-		});
+	// async loadParams() {
+	// 	return new Promise((resolve, reject) => {
+	// 		fetch('./weight.json')
+	// 			.then(response => {
+	// 				console.log("Getting weights: " + response.url);
+	// 				return response.json();
+	// 			})
+	// 			.then(data => {
+	// 				console.log("Got weights: " + data.length);
+	// 				this.setWeightData(data);
+	// 				fetch('./bias.json')
+	// 					.then(response => {
+	// 						console.log("Getting biases: " + response.url);
+	// 						return response.json();
+	// 					})
+	// 					.then(data => {
+	// 						console.log("Got biases: " + data.length);
+	// 						this.setBiasData(data);
+	// 						resolve();
+	// 					});
+	// 			});
+	// 	});
+	// }
+	loadParams(wegihtJSON, biasJSON) {
+		this.setWeightData(wegihtJSON);
+		this.setBiasData(biasJSON);
 	}
 	setWeightData(data) {
 		for (let i = 0; i < this.W.length; i++)
--- src/routes/capture.jsx
+++ src/routes/capture.jsx
@@ -1,13 +1,15 @@
 import React from 'react';
 import { Matrix, MulticlassClassifier } from '../ml.js';
+import trainedModel from '../training.js';
 
 class Capture extends React.Component {
 	constructor(props) {
 		console.clear();
 		super(props);
-		this.state = {
-			classifier: new MulticlassClassifier(null, null, [3, 16, 16, 3]),
-		};
+		// this.state = {
+		// 	// classifier: new MulticlassClassifier(null, null, [3, 16, 16, 3]),
+		// };
+		this.captureImage = this.captureImage.bind(this);
 	}
 	makeVideo() {
 		navigator.mediaDevices.getUserMedia({ video: true, audio: false })
@@ -25,6 +27,50 @@ class Capture extends React.Component {
 		);
 	}
 	captureImage() {
+		let { imageData, size } = this.getImageData();
+		let scaledImageData = this.getScaledImageData(imageData, size);
+		this.printImageData(scaledImageData);
+		let classifiedImageData = this.getClassifiedImageData(scaledImageData);
+		this.printClassifiedImageData(classifiedImageData);
+		let slicedClassifiedImageData = this.getSlicedClassifiedImageData(classifiedImageData);
+		let averagedClassifiedImageData = this.getAveragedClassifiedImageData(slicedClassifiedImageData);
+		let side = this.getSide(averagedClassifiedImageData);
+		console.log(side);
+	}
+	getSide(averagedClassifiedImageData) {
+		let side = new Matrix(3, 3);
+		for (let i = 0; i < 3; i++) {
+			for (let j = 0; j < 3; j++) {
+				side.data[i][j] = averagedClassifiedImageData[i * 3 + j].data[0][0];
+			}
+		}
+		return side.reshape(1, 9).data;
+	}
+	getAveragedClassifiedImageData(slicedClassifiedImageData) {
+		let averagedClassifiedImageData = [];
+		for (let i = 0; i < slicedClassifiedImageData.length; i++) {
+			// get the most frequent class
+			let frequency = slicedClassifiedImageData[i].maxFrequency();
+			averagedClassifiedImageData.push(frequency);
+		}
+		return averagedClassifiedImageData;
+	}
+	getSlicedClassifiedImageData(classifiedImageData) {
+		// reshape into a square
+		let size = Math.sqrt(classifiedImageData.data[0].length);
+		console.log("size", size);
+		classifiedImageData = classifiedImageData.reshape(size, size);
+		// slice
+		let sliced = [];
+		let squareSize = size / 3;
+		for (let i = 0; i < 3; i++) {
+			for (let j = 0; j < 3; j++) {
+				sliced.push(classifiedImageData.slice(i * squareSize, (i + 1) * squareSize, j * squareSize, (j + 1) * squareSize));	
+			}
+		}
+		return sliced;
+	}
+	getImageData() {
 		// Get the Image
 		let video = document.getElementById('video');
 		let canvas = document.getElementById('canvas');
@@ -33,18 +79,58 @@ class Capture extends React.Component {
 		let context = canvas.getContext('2d');
 		let w = video.videoWidth;
 		let h = video.videoHeight;
-		context.drawImage(video, (w-h)/2,0,h,h,  0,0,h,h);
+		context.drawImage(video, (w - h) / 2, 0, h, h, 0, 0, h, h);
 		let size = canvas.width;
 		let image = context.getImageData(0, 0, size, size);
 		let imageData = image.data;
-		
-		// Parse the data
+		return { imageData, size };
+	}
+	printImageData(imageData) {
+		// print to output
+		let output = document.getElementById('output');
+		let outPutString = "";
+		for (let i = 0; i < imageData.length; i++) {
+			for (let j = 0; j < imageData[i].length; j++) {
+				outPutString += `<span style="color:rgb(${imageData[i][j][0]},${imageData[i][j][1]},${imageData[i][j][2]})">█</span>`;
+			}
+			outPutString += "<br/>";
+		}
+		output.innerHTML = outPutString;
+	}
+	getClassifiedImageData(imageData) {
+		let X = this.imageDataToX(imageData);
+		let y = trainedModel.predict(X);
+		let {result, index} = y.maxDim("row");
+		return index;
+	}
+	printClassifiedImageData(imageData) {
+		let output = document.getElementById('output');
+		let outPutString = "<br/><br/>";
+		let colors = ['red', 'orange', 'yellow', 'green', 'blue', 'black', 'white'];
+		let size = Math.sqrt(imageData.data[0].length);
+		for (let i = 0; i < size; i++) {
+			for (let j = 0; j < size; j++) {
+				outPutString += `<span style="color:${colors[imageData.get(i * size + j)]}">█</span>`;
+			}
+			outPutString += "<br/>";
+		}
+		output.innerHTML = outPutString;
+	}
+	imageDataToX(imageData) {
+		let X = new Matrix(imageData.length, imageData[0].length);
+		X.data = imageData;
+		X = X.unravel();
+		X.data = X.data.map(pixel => [pixel[0][0], pixel[0][1], pixel[0][2]]);
+		X.cols = 3;
+		return X.T();
+	}
+	getScaledImageData(imageData, size) {
 		let data = [];
 		for (let i = 0; i < size; i++) {
 			data.push([]);
 			for (let j = 0; j < size; j++) {
 				let index = (i * size + j) * 4;
-				data[i].push([imageData[index], imageData[index+1], imageData[index+2]]);
+				data[i].push([imageData[index], imageData[index + 1], imageData[index + 2]]);
 			}
 		}
 		// scale
@@ -52,24 +138,11 @@ class Capture extends React.Component {
 		let resolusion = 16;
 		for (let i = 0; i < size; i += resolusion) {
 			let row = [];
-			for (let j = 0; j < size; j += resolusion) {
+			for (let j = 0; j < size; j += resolusion)
 				row.push(data[i][j]);
-			}
 			scaled.push(row);
 		}
-		// print to output
-		let output = document.getElementById('output');
-		let outPutString = "";
-		for (let i = 0; i < scaled.length; i++) {
-			for (let j = 0; j < scaled[i].length; j++) {
-				outPutString += `<span style="color:rgb(${scaled[i][j][0]},${scaled[i][j][1]},${scaled[i][j][2]})">█</span>`;
-			}
-			outPutString += "<br/>";
-		}
-		output.innerHTML = outPutString;
-
-		// Classify
-
+		return scaled;
 	}
 
 	render() {
