fd1f5a50b55eaa82695b5dacc23c4af21913f076
Om Mahesh <2om.mahesh@gmail.com>
2022-02-15 20:15:55 -0800

    thing

---
+++ src/cube/js/data/colors.js
@@ -0,0 +1,41 @@
+// export const COLORS = {
+//   "front": ["r", "b", "w", "b", "w", "w", "y", "o", "y"],
+//   "left": ["g", "g", "g", "r", "r", "w", "r", "r", "o"],
+//   "back": ["o", "w", "y", "w", "y", "y", "w", "y", "y"],
+//   "right": ["b", "g", "w", "r", "o", "g", "g", "b", "r"],
+//   "up": ["o", "o", "g", "o", "g", "r", "w", "o", "o"],
+//   "down": ["b", "y", "r", "b", "b", "y", "b", "g", "b"]
+// };
+
+export const COLORS = {
+  "front": ["g", "g", "g", "g", "g", "g", "g", "g", "g"],
+  "left": ["r", "r", "r", "r", "r", "r", "r", "r", "r"],
+  "back": ["b", "b", "b", "b", "b", "b", "b", "b", "b"],
+  "right": ["o", "o", "o", "o", "o", "o", "o", "o", "o"],
+  "up": ["y", "y", "y", "y", "y", "y", "y", "y", "y"],
+  "down": ["w", "w", "w", "w", "w", "w", "w", "w", "w"]
+};
+
+export const colorToHex = {
+  w: '#ffffff',
+  b: '#0045ad',
+  g: '#009b48',
+  r: '#b90000',
+  o: '#ff5900',
+  y: '#ffd500',
+  l: '#000000',
+  a: '#333333',
+};
+
+// let colors = ['red', 'orange', 'yellow', 'green', 'blue', 'black', 'white'];
+
+// export const colorToHex = [
+//   '#ffffff',
+//   '#0045ad',
+//   '#009b48',
+//   '#b90000',
+//   '#ff5900',
+//   '#ffd500',
+//   '#000000',
+//   '#333333',
+// ];
